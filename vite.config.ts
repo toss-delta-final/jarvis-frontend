bb83f57431decad19d5c73ad862a8a910f5a4668
Sohee Lee <essen030216@naver.com>
2026-07-18 18:28:06 +0900

    chore: 백엔드 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_PROXY_TARGET || "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
   },
   resolve: {
     alias: {
